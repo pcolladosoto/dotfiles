b5d544481e42bfb598d95b51405d0116d8464cc1
Pablo Collado <pcolladosoto@gmail.com>
2020-06-07 12:31:04 +0200

    Added code samples

---
+++ Cool_code_samples/Sudoku_solver.py
@@ -0,0 +1,43 @@
+# Taken form https://stackoverflow.com/questions/1697334/algorithm-for-solving-sudoku -- Answered by hari
+# It implements a backtracking algorithm, i.e, it tries a solution and then goes with
+# another one if the previous didn't work... It recursively calls a solving function!
+
+# Another useful link: https://warwick.ac.uk/fac/sci/moac/people/students/peter_cock/python/sudoku/
+
+def findNextCellToFill(grid, i, j):
+        for x in range(i,9):
+                for y in range(j,9):
+                        if grid[x][y] == 0:
+                                return x,y
+        for x in range(0,9):
+                for y in range(0,9):
+                        if grid[x][y] == 0:
+                                return x,y
+        return -1,-1
+
+def isValid(grid, i, j, e):
+        rowOk = all([e != grid[i][x] for x in range(9)])
+        if rowOk:
+                columnOk = all([e != grid[x][j] for x in range(9)])
+                if columnOk:
+                        # finding the top left x,y co-ordinates of the section containing the i,j cell
+                        secTopX, secTopY = 3 *(i//3), 3 *(j//3) #floored quotient should be used here. 
+                        for x in range(secTopX, secTopX+3):
+                                for y in range(secTopY, secTopY+3):
+                                        if grid[x][y] == e:
+                                                return False
+                        return True
+        return False
+
+def solveSudoku(grid, i=0, j=0):
+        i,j = findNextCellToFill(grid, i, j)
+        if i == -1:
+                return True
+        for e in range(1,10):
+                if isValid(grid,i,j,e):
+                        grid[i][j] = e
+                        if solveSudoku(grid, i, j):
+                                return True
+                        # Undo the current cell for backtracking
+                        grid[i][j] = 0
+        return False
